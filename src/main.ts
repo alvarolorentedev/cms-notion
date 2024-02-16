7be2492c0c48c06fb0fbb774e520d80c135bc12e
kanekotic <me@kanekotic.com>
2024-02-16 21:57:15 +0100

    feat: initial bundle

--- src/main.ts
+++ src/main.ts
@@ -1,45 +1,38 @@
-const { Client } = require("@notionhq/client");
-const { NotionToMarkdown } = require("notion-to-md");
+const { Client } = require('@notionhq/client')
+const { NotionToMarkdown } = require('notion-to-md')
 const core = require('@actions/core')
-const fs = require('fs');
+const fs = require('fs')
 const path = require('path')
-const yaml = require('js-yaml');
+const yaml = require('js-yaml')
 
 const propertyMapper = async (value: any) => {
-    if(value.type === "title")
-        return value.title[0].text.content
-    if(value.type === "number")
-        return value.number
-    if(value.type === "email")
-        return value.email
-    if(value.type === "url")
-        return value.url
-    if(value.type === "select")
-        return value.select.name
-    if(value.type === "files"){
-        const file = value.files[0]
-        if (file.type === "file")
-            return file.file.url
-        if (file.type === "external")
-            return file.external.url
-    }
-    if(value.type === "status")
-        return value.status.name
-    if(value.type === "date")
-        return new Date(value.date.start)
-    if(value.type === "created_time")
-        return new Date(value.created_time)
-    if(value.type === "checkbox")
-        return value.checkbox
-    if(value.type === "multi_select")
-        return value.multi_select.map((multi: any) => multi.name)
-    if(value.type === "created_by" || value.type === "people" || value.type === "last_edited_by")
-        return undefined
-    return JSON.stringify(value)
+  if (value.type === 'title') return value.title[0].text.content
+  if (value.type === 'number') return value.number
+  if (value.type === 'email') return value.email
+  if (value.type === 'url') return value.url
+  if (value.type === 'select') return value.select.name
+  if (value.type === 'files') {
+    const file = value.files[0]
+    if (file.type === 'file') return file.file.url
+    if (file.type === 'external') return file.external.url
+  }
+  if (value.type === 'status') return value.status.name
+  if (value.type === 'date') return new Date(value.date.start)
+  if (value.type === 'created_time') return new Date(value.created_time)
+  if (value.type === 'checkbox') return value.checkbox
+  if (value.type === 'multi_select')
+    return value.multi_select.map((multi: any) => multi.name)
+  if (
+    value.type === 'created_by' ||
+    value.type === 'people' ||
+    value.type === 'last_edited_by'
+  )
+    return undefined
+  return JSON.stringify(value)
 }
 
 function fillTemplate(template: any, data: any) {
-  return template.replace(/{(\w+)}/g, (match:any, key:any) => data[key] || '');
+  return template.replace(/{(\w+)}/g, (match: any, key: any) => data[key] || '')
 }
 
 export async function run() {
@@ -47,39 +40,48 @@ export async function run() {
     required: true
   })
 
-  const notion = new Client({ 
+  const notion = new Client({
     auth: core.getInput('notion-api-key', { required: true })
-  });
+  })
   const posts = await notion.databases.query({
     filter: {
-        "and": [
-            {
-                "property": "scheduled",
-                "date": {
-                    "on_or_before": new Date().toISOString()
-                }
-            },
-            {
-                "property": "draft",
-                "checkbox": {
-                    "equals": false
-                }
-            }
-        ]
+      and: [
+        {
+          property: 'scheduled',
+          date: {
+            on_or_before: new Date().toISOString()
+          }
+        },
+        {
+          property: 'draft',
+          checkbox: {
+            equals: false
+          }
+        }
+      ]
     },
-    database_id: core.getInput('notion-database-id', {required: true })
-});
-const n2m = new NotionToMarkdown({ notionClient: notion });
-posts.results.foreach(async (post: any) => {
-    const mdblocks = await n2m.pageToMarkdown(post.id);
-    const mdString = n2m.toMarkdownString(mdblocks);
-    const propEntries = Object.fromEntries((await Promise.all(Object.entries(post.properties || [])
-    .map(async ([name, value]) => ([name, await propertyMapper(value)]))
-)).filter(([_, value]) => value))
-    const yamlFrontMatter = yaml.dump(propEntries);
-    const frontMatterDelimiter = core.getInput('front-matter-delimiter', {required: true })
-    const content = `${frontMatterDelimiter}\n${yamlFrontMatter}${frontMatterDelimiter}\n${mdString.parent}`;
-    console.log(content);
+    database_id: core.getInput('notion-database-id', { required: true })
+  })
+  const n2m = new NotionToMarkdown({ notionClient: notion })
+  posts.results.foreach(async (post: any) => {
+    const mdblocks = await n2m.pageToMarkdown(post.id)
+    const mdString = n2m.toMarkdownString(mdblocks)
+    const propEntries = Object.fromEntries(
+      (
+        await Promise.all(
+          Object.entries(post.properties || []).map(async ([name, value]) => [
+            name,
+            await propertyMapper(value)
+          ])
+        )
+      ).filter(([_, value]) => value)
+    )
+    const yamlFrontMatter = yaml.dump(propEntries)
+    const frontMatterDelimiter = core.getInput('front-matter-delimiter', {
+      required: true
+    })
+    const content = `${frontMatterDelimiter}\n${yamlFrontMatter}${frontMatterDelimiter}\n${mdString.parent}`
+    console.log(content)
     const destinationFolder = core.getInput('destination-folder', {
       required: true
     })
@@ -91,6 +93,6 @@ posts.results.foreach(async (post: any) => {
       destinationFolder,
       `${fillTemplate(fileNameFormat, propEntries)}.md`
     )
-    fs.writeFileSync(destinationFilePath,content)
-})
+    fs.writeFileSync(destinationFilePath, content)
+  })
 }
